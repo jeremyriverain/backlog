f4eeafc75ac7a516850c6314defc067810be4261
Jérémy Riverain <jeremy@geekco.fr>
2021-02-19 21:08:51 +0100

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div>hello world</div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    // const meta = ref<Meta>({
+    //   totalCount: 1200,
+    // })
+    // return { todos, meta }
+  },
+})
+</script>
